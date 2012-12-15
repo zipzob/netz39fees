ca8ebd3e00c0d80604e134dd01f21369824a24f3
Andreas Pfohl <andreas@pf0hl.de>
2012-12-15 23:03:51 +0100

    Check that donation is a number

--- app/models/fee.rb
+++ app/models/fee.rb
@@ -1,4 +1,5 @@
 class Fee < ActiveRecord::Base
   attr_accessible :donation, :fee, :name
   validates :name, :fee, presence: true, on: :create
+  validates :donation, numericality: true, unless: Proc.new { |a| a.donation.blank? }, on: :create
 end
